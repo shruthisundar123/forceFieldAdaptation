fixed distance to line formula

train_CF1.py
@@ -13,6 +13,9 @@ from plotting_functions import print_training_log
 from test_network import null_test
 from test_network import curl_test
 
+########################################
+## SETUP
+
 # Recreate the model
 nn = create_network()
 
@@ -21,47 +24,47 @@ weight_file = "save_NF1" + os.path.sep + "weights.h5"
 nn.load_weights(weight_file) #.expect_partial()
 
 ########################################
-# TRAIN NETWORK IN A CURL FIELD
-
-condition = "adapt" # to re-learn centre-out reaches in a given FF/NF
+# TRAIN NULL-TRAINED NETWORK IN A CURL FIELD
 
 n_t = 100
 n_batches = 1 #256
-batch_size = 1 #64 #change to 1 when actually adapting to represent human learning more
+batch_size = 1 #64 
 
-#to store results after each fit
-myLargeArr = np.empty((0, 100, 4), float)
+#to store test targets and results after each fit
+myResultsArr = np.empty((0, 100, 4), float) 
+myTargetsArr = np.empty((0, 100, 4), float)
 
 # this callback logs training information for each batch passed, rather than for each epoch.
 callbacks = [mn.nets.callbacks.BatchLogger()]
 for i in range(2000):
-    #generate the inputs and initial states for the curl field
-    # ff_coefficient=8 makes it the curl field
-    print(i + 1)
+    print(i + 1) #current training number
+
+    ## TRAINING
+    condition = "adapt" # to re-learn centre-out reaches in a given FF/NF   
     nn.task.angular_step = 15 # reset to original amount
-    [inputs, targets, init_states] = nn.task.generate(n_timesteps=n_t, batch_size=batch_size*n_batches, condition=condition, ff_coefficient=8) #coefficient = 8 for curl field
+
+    # generate inputs and targets, fit the model
+    [inputs, targets, init_states] = nn.task.generate(n_timesteps=n_t, batch_size=batch_size*n_batches, condition=condition, ff_coefficient=8) #ff_coefficient = 8 for curl field
     h = nn.fit(x=[inputs, init_states], y=targets, verbose=1, epochs=1, batch_size=batch_size, shuffle=False, callbacks=callbacks)
 
+    ## TESTING
     ## collect results to assess learning curve
-    #get results
+    condition = "test"
     n_mov_circle = 8 # number of movement directions around the unit circle
     n_t = 100
     nn.task.angular_step = 360 / n_mov_circle
-
+    
+    # generate inputs and get model's predictions
     [inputs, targets, init_states] = nn.task.generate(n_timesteps=n_t, batch_size=n_mov_circle, condition=condition)
     results = nn([inputs, init_states], training=False)
 
-    #store results 
-    tempArr = np.array(results['cartesian position'])
-    myLargeArr = np.append(myLargeArr, tempArr, axis=0)
+    #store targets and results for this fit
+    myResultsArr = np.append(myResultsArr, results['cartesian position'], axis=0)
+    myTargetsArr = np.append(myTargetsArr, targets, axis=0)
 
 ######################################
 # SAVE MODEL PARAMETERS
 folderLocation = "save_CF1" + os.path.sep
-
-# view training log
-training_log = callbacks[0].history
-print_training_log(folderLocation, log=training_log)
 
 # save the trained model
 weight_file = folderLocation + "weights.h5"
@@ -70,8 +73,11 @@ log_file = folderLocation + "log.json"
 # save model weights
 nn.save_weights(weight_file, save_format='h5')
 
+# save training targets and results after each fit
 cartesian_results_filename = folderLocation + "cartesian_position.npy"
-np.save(cartesian_results_filename, myLargeArr)
+targets_filename = folderLocation + "targets.npy"
+np.save(cartesian_results_filename, myResultsArr)
+np.save(targets_filename, myTargetsArr)
 
 # save training history (log)
 with open(log_file, 'w') as file:
@@ -81,7 +87,11 @@ with open(log_file, 'w') as file:
 print("Done saving curl trained model.")
 
 ########################################
-# TESTING THE CURL-TRAINED NETWORK
+# TESTING THE CURL-TRAINED NETWORK AND VIEW AND SAVE RESULTS
+
+# view training log
+training_log = callbacks[0].history
+print_training_log(folderLocation, log=training_log)
 
 null_test(folderLocation, nn) #test in a null field
 curl_test(folderLocation, nn) #test in a curl field

train_NF1.py
@@ -14,25 +14,27 @@ from test_network import null_test
 from test_network import curl_test
 
 ######################################
-## create the network
+## CREATE THE NETWORK
 nn = create_network()
 
 ####################################
-# TRAIN NETWORK IN A NULL FIELD
+## TRAIN NETWORK IN A NULL FIELD
 
 n_t = 100
 n_batches = 256
 batch_size = 64
 
-#to store results after each fit
-myLargeArr = np.empty((0, 100, 4), float)
-myTargetArr = np.empty((0, 100, 4), float)
-
+#to store test targets and results after each fit
+# 100 data points in a single reach; each data point has 4 parameters (x, y, shoulder velocity, elbow velocity)
+myResultsArr = np.empty((0, 100, 4), float) 
+myTargetsArr = np.empty((0, 100, 4), float)
 
 # this callback logs training information for each batch passed, rather than for each epoch.
 callbacks = [mn.nets.callbacks.BatchLogger()]
 for i in range(30):
     print(i + 1)
+
+    ## TRAINING
     condition = "train"
     nn.task.angular_step = 15 # reset to original amount
     
@@ -42,27 +44,25 @@ for i in range(30):
     # verbose = 1 will print the training losses
     h = nn.fit(x=[inputs, init_states], y=targets, verbose=1, epochs=1, batch_size=batch_size, shuffle=False, callbacks=callbacks)
 
-    ## collect results to assess learning curve
-    #get results
+    ## TESTING
+    # collect results to assess learning curve
     condition = "test"
     n_mov_circle = 8 # number of movement directions around the unit circle
     n_t = 100
     nn.task.angular_step = 360 / n_mov_circle
 
+    # generate inputs and get model's predictions
     [inputs, targets, init_states] = nn.task.generate(n_timesteps=n_t, batch_size=n_mov_circle, condition=condition)
     results = nn([inputs, init_states], training=False)
 
-    #store results 
-    myLargeArr = np.append(myLargeArr, results['cartesian position'], axis=0)
-    myTargetArr = np.append(myTargetArr, targets, axis=0)
+    #store targets and results for this fit
+    myResultsArr = np.append(myResultsArr, results['cartesian position'], axis=0)
+    myTargetsArr = np.append(myTargetsArr, targets, axis=0)
 
 ######################################
-# SAVE MODEL PARAMETERS
-folderLocation = "save_NF1" + os.path.sep
+## SAVE MODEL PARAMETERS
 
-# view training log
-training_log = callbacks[0].history
-print_training_log(folderLocation, log=training_log)
+folderLocation = "save_NF1" + os.path.sep
 
 # save the trained model
 weight_file = folderLocation + "weights.h5"
@@ -73,10 +73,11 @@ log_file = folderLocation + "log.json"
 # save model weights
 nn.save_weights(weight_file, save_format='h5')
 
+# save training targets and results after each fit
 cartesian_results_filename = folderLocation + "cartesian_position.npy"
 targets_filename = folderLocation + "targets.npy"
-np.save(cartesian_results_filename, myLargeArr)
-np.save(targets_filename, myTargetArr)
+np.save(cartesian_results_filename, myResultsArr)
+np.save(targets_filename, myTargetsArr)
 
 # save model configuration ### NOT NEEDED
 # if not os.path.isfile(cfg_file + ".json"):
@@ -90,7 +91,11 @@ with open(log_file, 'w') as file:
 print("Done saving null trained model.")
 
 ########################################
-# TESTING THE NULL-TRAINED NETWORK
+## TESTING THE NULL-TRAINED NETWORK AND VIEW AND SAVE RESULTS
+
+# view training log
+training_log = callbacks[0].history
+print_training_log(folderLocation, log=training_log)
 
 null_test(folderLocation, nn) #test in a null field
 curl_test(folderLocation, nn) #test in a curl field